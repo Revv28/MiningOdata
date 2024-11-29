4386791a5e1ae17da2845cad43d666e43ece2fef
CHINTHA RAGHU VAMSI KRISHNA REDDY <chintha-raghu-vamsi-krishna.reddy@capgemini.com>
2024-11-29 05:42:56 +0000

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("app.mining0953.controller.View1", {
+        onInit() {
+        },
+        onRowSelect:function(oEvt){
+            // var oItem = oEvt.getParameter("listItem");
+            // var sPath = oItem.getBindingContextPath();
+            // var entity = sPath + '/mining_to_drill';
+            // var oModel = this.getOwnerComponent().getModel();
+            // var parameters = {
+            //     success:function(oData){
+            //         console.log(oData);
+                    
+            //     },
+            //     error:function(error){
+
+            //     }
+            // }
+            // oModel.read(entity,parameters)
+        },
+        onButtonPress:function(){
+            
+        }
+
+
+    });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["app/mining0953/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
